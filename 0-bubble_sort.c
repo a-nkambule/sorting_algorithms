5a4b9ff202b202aeb11d3d1eb4cc72e2b4be3172
Aviwe Nkambule <aviwenkambule@gmail.com>
2024-02-16 04:53:52 -0800

    sorting_algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,46 +6,47 @@
  * @size: The size of the array
  * Description: Prints the array after each swap
  */
-void bubble_sort(int *array, size_t size) 
+void bubble_sort(int *array, size_t size)
 {
-    int i, j;
-
-    for (i = 0; i < size - 1; i++) {
-        for (j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j+1]) {
-                // Swap two elements
-                int temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
-
-                // Print the array after each swap
-                for (int k = 0; k < size; k++) {
-                    printf("%d ", array[k]);
-                }
-                printf("\n");
-            }
-        }
-    }
+int i, j;
+for (i = 0; i < size - 1; i++)
+{
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+int temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+for (int k = 0; k < size; k++)
+{
+printf("%d ", array[k]);
 }
-
-int main() 
+printf("\n");
+}
+}
+}
+}
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main(void)
 {
-    int array[] = {5, 3, 8, 2, 1};
-    size_t size = sizeof(array) / sizeof(array[0]);
-
-    printf("Original array: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", array[i]);
-    }
-    printf("\n\n");
-
-    bubble_sort(array, size);
-
-    printf("\nSorted array: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
-
-    return 0;
+int array[] = {5, 3, 8, 2, 1};
+size_t size = sizeof(array) / sizeof(array[0]);
+printf("Original array: ");
+for (int i = 0; i < size; i++)
+{
+printf("%d ", array[i]);
+}
+printf("\n\n");
+bubble_sort(array, size);
+printf("\nSorted array: ");
+for (int i = 0; i < size; i++)
+{
+printf("%d ", array[i]);
+}
+printf("\n");
+return (0);
 }
