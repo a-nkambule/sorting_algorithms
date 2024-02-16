71704d7cf49c585304970115e03d89c1bc2fda7d
Aviwe Nkambule <aviwenkambule@gmail.com>
2024-02-16 06:17:52 -0800

    sorting_algorithms

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -25,6 +25,7 @@ else
  * insertion_sort_list - Sorts a doubly linked list of integers
  * in ascending order using the Insertion sort algorithm
  * @list: Double pointer to the head of the list
+ * Description: prints the list after each swap
  */
 void insertion_sort_list(listint_t **list)
 {
