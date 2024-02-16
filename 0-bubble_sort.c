8ce99142dbe94c985c77e7e3792b3fed51fc1955
Aviwe Nkambule <aviwenkambule@gmail.com>
2024-02-16 05:11:23 -0800

    sorting_algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,19 @@
 #include "sort.h"
 
 /**
+ * swap_ints - swap two integers in an array
+ * @a: the first integer to swap
+ * @b: the second integer to swap
+ */
+void swap_ints(int *a, int *b)
+{
+int tmp;
+tmp = *a;
+*a = *b;
+*b = tmp;
+}
+
+/**
  * bubble_sort - Sort an array of intergers in ascending order
  * @array: An array of integers to sort
  * @size: The size of the array
@@ -8,45 +21,22 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-int i, j;
-for (i = 0; i < size - 1; i++)
+size_t j, len = size;
+bool bubbly = false;
+if (array == NULL || size < 2)
+return;
+while (bubbly == false)
 {
-for (j = 0; j < size - i - 1; j++)
+bubbly = true;
+for (j = 0; j < len - 1; j++)
 {
 if (array[j] > array[j + 1])
 {
-int temp = array[j];
-array[j] = array[j + 1];
-array[j + 1] = temp;
-for (int k = 0; k < size; k++)
-{
-printf("%d ", array[k]);
-}
-printf("\n");
+swap_ints(array + j, array + j + 1);
+print_array(array, size);
+bubbly = false;
 }
 }
+len--;
 }
-}
-/**
- * main - entry point
- * Return: Always 0
- */
-int main(void)
-{
-int array[] = {5, 3, 8, 2, 1};
-size_t size = sizeof(array) / sizeof(array[0]);
-printf("Original array: ");
-for (int i = 0; i < size; i++)
-{
-printf("%d ", array[i]);
-}
-printf("\n\n");
-bubble_sort(array, size);
-printf("\nSorted array: ");
-for (int i = 0; i < size; i++)
-{
-printf("%d ", array[i]);
-}
-printf("\n");
-return (0);
 }
