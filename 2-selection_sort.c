bc1d3cbcaddf962f5155a573b21eef71a5e21cb4
Aviwe Nkambule <aviwenkambule@gmail.com>
2024-02-20 03:29:36 -0800

    sorting_algorithms

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,10 +15,9 @@ tmp = *a;
 
 /**
  * selection_sort - Sort an array of integers in ascending order
- *                  using the selection sort algorithm.
+ * using the selection sort algorithm.
  * @array: An array of integers.
  * @size: The size of the array.
- *
  * Description: Prints the array after each swap.
  */
 void selection_sort(int *array, size_t size)
@@ -29,12 +28,12 @@ if (array == NULL || size < 2)
 return;
 for (j = 0; j < size - 1; j++)
 {
-*min = array + j;
+min = array + j;
 for (k = j + 1; k < size; k++)
-*min = (array[k] < *min) ? (array + k) : *min;
-if ((array + j) != *min)
+min = (array[k] < *min) ? (array + k) : min;
+if ((array + j) != min)
 {
-swap_ints(array + j, *min);
+swap_ints(array + j, min);
 print_array(array, size);
 }
 }
