60d9d09f3f7454f8864972865e4eb8f22b0bbe1b
Aviwe Nkambule <aviwenkambule@gmail.com>
2024-02-20 03:43:30 -0800

    sorting_algorithms

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -15,7 +15,7 @@ tmp = *a;
 
 /**
  * shell_sort - Sort an array of integers in ascending
- * order using the shell sort algorith
+ * order using the shell sort algorithm
  * @array: An array of integers
  * @size: The size of the array
  * Description: Uses the Knuth interval sequence
@@ -25,7 +25,7 @@ void shell_sort(int *array, size_t size)
 size_t gap, j, k;
 if (array == NULL || size < 2)
 return;
-for (gap = 1; gap < (size / 3);)
+for (gap = 1; gap < (size / 3));
 gap = gap * 3 + 1;
 for (; gap >= 1; gap /= 3)
 {
